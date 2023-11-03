310aa75e7b1b4d00846fc1b88d3b4eefa16c0dc2
alvarosaugarlr <alvaro.saugar@liferay.com>
2023-11-03 18:47:02 +0100

    LPS-190119 - solving problems in scheduler

--- modules/dxp/apps/scim/scim-configuration-web/src/main/java/com/liferay/scim/configuration/web/internal/scheduler/SCIMNotificationSchedulerJobConfiguration.java
+++ modules/dxp/apps/scim/scim-configuration-web/src/main/java/com/liferay/scim/configuration/web/internal/scheduler/SCIMNotificationSchedulerJobConfiguration.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -7,16 +7,13 @@ package com.liferay.scim.configuration.web.internal.scheduler;
 
 import com.liferay.mail.kernel.model.MailMessage;
 import com.liferay.mail.kernel.service.MailService;
-import com.liferay.oauth2.provider.exception.NoSuchOAuth2ApplicationException;
 import com.liferay.oauth2.provider.model.OAuth2Application;
 import com.liferay.oauth2.provider.model.OAuth2Authorization;
 import com.liferay.oauth2.provider.service.OAuth2ApplicationLocalService;
 import com.liferay.oauth2.provider.service.OAuth2AuthorizationLocalService;
-import com.liferay.petra.function.UnsafeConsumer;
 import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.petra.function.transform.TransformUtil;
-import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONUtil;
@@ -39,56 +36,40 @@ import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.service.UserNotificationEventLocalService;
 import com.liferay.portal.kernel.util.DateUtil;
-import com.liferay.portal.kernel.util.PrefsProps;
-import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.OrderByComparatorFactoryUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.scim.client.configuration.SCIMClientOAuth2ApplicationConfiguration;
-import com.liferay.scim.client.util.SCIMClientUtil;
-import com.liferay.scim.configuration.web.internal.constants.SCIMConstants;
 import com.liferay.scim.configuration.web.internal.constants.SCIMWebKeys;
-import org.osgi.framework.BundleContext;
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
+
+import java.io.IOException;
 
 import java.text.SimpleDateFormat;
+
 import java.util.Date;
-import java.util.Dictionary;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 import javax.mail.internet.InternetAddress;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Alvaro Saugar
  */
-@Component(
-	service = SchedulerJobConfiguration.class
-)
+@Component(service = SchedulerJobConfiguration.class)
 public class SCIMNotificationSchedulerJobConfiguration
 	implements SchedulerJobConfiguration {
 
+	public static final int DAY = 1;
 
+	public static final int MONTH = 30;
 
-
-	@Override
-	public UnsafeConsumer<Long, Exception>
-	getCompanyJobExecutorUnsafeConsumer() {
-
-		System.out.println("entra en getCompanyJobExecutorUnsafeConsumer");
-		return null;
-		//return companyId -> _updateMetadata(companyId);
-	}
+	public static final int WEEK = 7;
 
 	@Override
 	public UnsafeRunnable<Exception> getJobExecutorUnsafeRunnable() {
-
-		System.out.println("entra en getJobExecutorUnsafeRunnable ");
-
 		return () -> _companyLocalService.forEachCompany(
 			company -> {
 				if (!company.isActive()) {
@@ -97,175 +78,59 @@ public class SCIMNotificationSchedulerJobConfiguration
 
 				_sendNotification(company.getCompanyId());
 			});
+	}
 
-
+	public OrderByComparator<OAuth2Authorization> getOrderByComparator() {
+		return OrderByComparatorFactoryUtil.create(
+			"OAuth2Authorization", "accessTokenExpirationDate", "asc");
 	}
 
 	@Override
 	public TriggerConfiguration getTriggerConfiguration() {
-		return TriggerConfiguration.createTriggerConfiguration(
-			1, TimeUnit.MINUTE);
-		/*return TriggerConfiguration.createTriggerConfiguration(
-			_samlConfiguration.getSpMessageCheckInterval(), TimeUnit.MINUTE);
-
-		 */
+		return TriggerConfiguration.createTriggerConfiguration(1, TimeUnit.DAY);
 	}
 
-	@Activate
-	protected void activate(Map<String, Object> properties) {
+	protected ClassLoader getClassLoader() {
+		Class<?> clazz = getClass();
 
-		_scimClientOAuth2ApplicationConfiguration = ConfigurableUtil.createConfigurable(
-			SCIMClientOAuth2ApplicationConfiguration.class, properties);
+		return clazz.getClassLoader();
 	}
 
+	private String _generateBody(String strAccessTokenExpirationDate) {
+		String body = StringPool.BLANK;
 
-	private void _sendNotification(long companyId) {
-		
-		String clientId = SCIMClientUtil.generateSCIMClientId(
-			_scimClientOAuth2ApplicationConfiguration.applicationName());
-
-		OAuth2Application oAuth2Application = null;
 		try {
-			oAuth2Application =
-				_oAuth2ApplicationLocalService.getOAuth2Application(
-					companyId, clientId);
+			body = StringUtil.read(
+				getClassLoader(),
+				"com/liferay/scim/configuration/web/internal/dependencies" +
+					"/body.tmpl");
 
-			List<OAuth2Authorization> applicationOAuth2Authorizations =
-				null;
-
-			applicationOAuth2Authorizations =
-				_oAuth2AuthorizationLocalService.getOAuth2Authorizations(
-					oAuth2Application.getOAuth2ApplicationId(),
-					QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
-
-			Date accessTokenDate = null;
-
-			boolean hasToSentNotification =
-				_hasToSentNotification(applicationOAuth2Authorizations,
-					accessTokenDate);
-
-
-			if (hasToSentNotification) {
-
-				Role role = null;
-				role = _roleLocalService.getRole(
-					companyId, RoleConstants.ADMINISTRATOR);
-
-				List<User> users =
-					_userLocalService.getRoleUsers(role.getRoleId());
-
-
-				SimpleDateFormat formatter =
-					new SimpleDateFormat("dd-MMM-yyyy");
-
-				String strDate = formatter.format(accessTokenDate);
-
-				Company company =
-					_companyLocalService.getCompany(companyId);
-
-				ResourceBundle resourceBundle =
-					ResourceBundleUtil.getBundle(
-						"content.Language", company.getLocale(),
-						getClass());
-				String subject = _language.get(
-					resourceBundle,
-					"scim-email-subject");
-
-
-				String body = _language.get(
-					resourceBundle,
-					"scim-email-body", strDate);
-
-
-				_sendNotificationEvent(users, role.getRoleId(), body);
-				_sendEmail(companyId, subject, body, users);
-
-			}
+			body = StringUtil.replace(
+				body, new String[] {"[$DATE_EXPIRATION_ACCESS_TOKEN$]"},
+				new String[] {strAccessTokenExpirationDate});
 		}
-		catch (Exception e) {
-			throw new RuntimeException(e);
+		catch (IOException ioException) {
+			throw new RuntimeException(ioException);
 		}
+
+		return body;
 	}
 
+	private void _sendEmail(
+			Company company, String subject, String body, List<User> users)
+		throws Exception {
 
-	private boolean _hasToSentNotification(List<OAuth2Authorization> applicationOAuth2Authorizations, Date accessTokenDate) {
-
-		Iterator<OAuth2Authorization> iterator =
-			applicationOAuth2Authorizations.iterator();
-
-		boolean sendNotification = false;
-
-		while (iterator.hasNext()) {
-			OAuth2Authorization oAuth2Authorization = iterator.next();
-
-			Date accessTokenExpirationDate =
-				oAuth2Authorization.getAccessTokenExpirationDate();
-
-			int daysBetween = DateUtil.getDaysBetween(
-				new Date(),
-				accessTokenExpirationDate);
-
-			if ((daysBetween == MONTH) || (daysBetween == WEEK) || (daysBetween == DAY)) {
-				sendNotification = true;
-			} else if (daysBetween > MONTH) {
-				return false;
-			}
-			if (DateUtil.compareTo(accessTokenDate, accessTokenExpirationDate) > 0){
-				accessTokenDate = accessTokenExpirationDate;
-			}
-
-		}
-
-		return sendNotification;
-	}
-
-	private void _sendNotificationEvent(List<User> users, long roleValue, String body)
-		 {
-
-			 try {
-			 	for (User user: users) {
-					 if (UserNotificationManagerUtil.isDeliver(
-						 user.getUserId(), SCIMWebKeys.SCIM_CONFIGURATION, 0,
-						 UserNotificationDefinition.NOTIFICATION_TYPE_ADD_ENTRY,
-						 UserNotificationDeliveryConstants.TYPE_WEBSITE)) {
-
-						 NotificationEvent notificationEvent = new NotificationEvent(
-							 System.currentTimeMillis(), SCIMWebKeys.SCIM_CONFIGURATION,
-							 JSONUtil.put(
-								 "body", body
-							 ));
-
-						 notificationEvent.setDeliveryType(
-							 UserNotificationDeliveryConstants.TYPE_WEBSITE);
-
-
-						 _userNotificationEventLocalService.addUserNotificationEvent(
-							 user.getUserId(), notificationEvent);
-						 }
-				 }
-			 }
-			 catch (PortalException e) {
-				 throw new RuntimeException(e);
-			 }
-		 }
-
-
-	private void _sendEmail(long companyId, String subject, String body, List<User> users) throws Exception {
-
-		String defaultEmailFromAddress = _prefsProps.getString(
-			companyId, PropsKeys.ADMIN_EMAIL_FROM_ADDRESS);
-
-		String defaultEmailFromName = _prefsProps.getString(
-			companyId, PropsKeys.ADMIN_EMAIL_FROM_NAME);
+		String defaultEmailFromAddress = "scim-notification@" + company.getMx();
+		String defaultEmailFromName = "SCIM-Notification";
 
 		InternetAddress from = new InternetAddress(
-			defaultEmailFromAddress, defaultEmailFromName);
+			defaultEmailFromName, defaultEmailFromAddress);
 
 		List<InternetAddress> bcc = TransformUtil.transform(
 			users,
 			user -> {
 				if (!StringUtil.equals(
-					user.getEmailAddress(), "test@liferay.com")) {
+						user.getEmailAddress(), "test@liferay.com")) {
 
 					try {
 						return new InternetAddress(
@@ -281,22 +146,123 @@ public class SCIMNotificationSchedulerJobConfiguration
 
 		MailMessage mailMessage = new MailMessage(from, subject, body, true);
 
-		InternetAddress[] bccArray = (InternetAddress[])bcc.toArray(new InternetAddress[0]);
-
-		mailMessage.setBCC(bccArray);
+		mailMessage.setBCC(
+			(InternetAddress[])bcc.toArray(new InternetAddress[0]));
 
 		_mailService.sendEmail(mailMessage);
 	}
 
+	private void _sendNotification(long companyId) {
+		List<OAuth2Application> oAuth2Applications = null;
+
+		try {
+			oAuth2Applications =
+				_oAuth2ApplicationLocalService.getOAuth2Applications(companyId);
+
+			for (OAuth2Application oAuth2Application : oAuth2Applications) {
+				if (oAuth2Application.getClientId(
+					).startsWith(
+						_SCIM_CLIENT_ID_PREFIX
+					)) {
+
+					List<OAuth2Authorization> applicationOAuth2Authorizations =
+						_oAuth2AuthorizationLocalService.
+							getOAuth2Authorizations(
+								oAuth2Application.getOAuth2ApplicationId(),
+								QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+								getOrderByComparator());
+
+					if (applicationOAuth2Authorizations != null) {
+						OAuth2Authorization applicationOAuth2Authorization =
+							applicationOAuth2Authorizations.get(0);
+
+						Date accessTokenExpirationDate =
+							applicationOAuth2Authorization.
+								getAccessTokenExpirationDate();
+
+						int daysBetween = DateUtil.getDaysBetween(
+							new Date(), accessTokenExpirationDate);
+
+						if ((daysBetween == MONTH) || (daysBetween == WEEK) ||
+							(daysBetween == DAY)) {
+
+							Role role = _roleLocalService.getRole(
+								companyId, RoleConstants.ADMINISTRATOR);
+
+							List<User> users = _userLocalService.getRoleUsers(
+								role.getRoleId());
+
+							SimpleDateFormat formatter = new SimpleDateFormat(
+								"dd-MMM-yyyy");
+
+							String strAccessTokenExpirationDate =
+								formatter.format(accessTokenExpirationDate);
+
+							Company company = _companyLocalService.getCompany(
+								companyId);
+
+							ResourceBundle resourceBundle =
+								ResourceBundleUtil.getBundle(
+									"content.Language", company.getLocale(),
+									getClass());
+
+							String subject = _language.get(
+								resourceBundle, "scim-email-subject");
+
+							String body = _generateBody(
+								strAccessTokenExpirationDate);
+
+							_sendNotificationEvent(users, body);
+							_sendEmail(company, subject, body, users);
+						}
+					}
+				}
+			}
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+	private void _sendNotificationEvent(List<User> users, String body) {
+		try {
+			for (User user : users) {
+				if (UserNotificationManagerUtil.isDeliver(
+						user.getUserId(), SCIMWebKeys.SCIM_CONFIGURATION, 0,
+						UserNotificationDefinition.NOTIFICATION_TYPE_ADD_ENTRY,
+						UserNotificationDeliveryConstants.TYPE_WEBSITE)) {
+
+					NotificationEvent notificationEvent = new NotificationEvent(
+						System.currentTimeMillis(),
+						SCIMWebKeys.SCIM_CONFIGURATION,
+						JSONUtil.put("body", body));
+
+					notificationEvent.setDeliveryType(
+						UserNotificationDeliveryConstants.TYPE_WEBSITE);
+
+					_userNotificationEventLocalService.addUserNotificationEvent(
+						user.getUserId(), notificationEvent);
+				}
+			}
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(portalException);
+		}
+	}
+
+	private static final String _SCIM_CLIENT_ID_PREFIX = "SCIM_";
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		SCIMNotificationSchedulerJobConfiguration.class);
 
-	public static final int MONTH = 30;
+	@Reference
+	private CompanyLocalService _companyLocalService;
 
-	public static final int WEEK = 7;
+	@Reference
+	private Language _language;
 
-	public static final int DAY = 1;
-	private SCIMClientOAuth2ApplicationConfiguration _scimClientOAuth2ApplicationConfiguration;
+	@Reference
+	private MailService _mailService;
 
 	@Reference
 	private OAuth2ApplicationLocalService _oAuth2ApplicationLocalService;
@@ -305,25 +271,13 @@ public class SCIMNotificationSchedulerJobConfiguration
 	private OAuth2AuthorizationLocalService _oAuth2AuthorizationLocalService;
 
 	@Reference
-	private UserNotificationEventLocalService
-		_userNotificationEventLocalService;
-
-	@Reference
-	private CompanyLocalService _companyLocalService;
-
-
-	@Reference
 	private RoleLocalService _roleLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
 
 	@Reference
-	private MailService _mailService;
+	private UserNotificationEventLocalService
+		_userNotificationEventLocalService;
 
-	@Reference
-	private PrefsProps _prefsProps;
-
-	@Reference
-	private Language _language;
 }
