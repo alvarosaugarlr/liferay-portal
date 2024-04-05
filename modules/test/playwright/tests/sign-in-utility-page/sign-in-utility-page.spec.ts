82b1a499a72288bdf3a474d0452f96a0bc81fa75
alvarosaugarlr <alvaro.saugar@liferay.com>
2024-04-05 14:56:35 +0200

    LPD-6871 more cases tests

--- modules/test/playwright/tests/sign-in-utility-page/sign-in-utility-page.spec.ts
+++ modules/test/playwright/tests/sign-in-utility-page/sign-in-utility-page.spec.ts
@@ -37,10 +37,19 @@ test('Forgot Pasword Utility Page created and set as default', async ({
 	await page.getByLabel('Desktop', { exact: true }).press('Enter');
 	await page.getByRole('menuitem', { name: 'Button Add Button Mark Button as Favorite' }).click();
 	await page.getByLabel('Desktop', { exact: true }).press('Enter');
-
 	await page.getByLabel('Publish', { exact: true }).click();
+
+	page.on('dialog', async (dialogWindow) => {
+		await dialogWindow.accept();
+	});
+
 	await page.locator('[id="_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_entries_5"]').getByLabel('More actions').click();
 	await page.getByRole('menuitem', { name: 'Mark as Default' }).click();
+	await page.waitForTimeout(3000);
+
+
+
+
 	await page.getByLabel('Test Test User Profile').click();
 	await page.getByRole('menuitem', { name: 'Sign Out' }).click();
 	await page.getByRole('button', { name: 'Sign In' }).click();
@@ -48,4 +57,101 @@ test('Forgot Pasword Utility Page created and set as default', async ({
 
 
 	expect(await page.getByRole('link', { name: 'Go Somewhere' }));
+})
+test('Forgot Pasword Utility Page created and set as non-default 3', async ({
+																			  page,
+																		  }) => {
+	await page.goto('/');
+	await page.getByLabel('Open Product Menu').click();
+	await page.getByRole('menuitem', { name: 'Site Builder' }).click();
+	await page.getByRole('menuitem', { name: 'Pages' }).click();
+	await page.getByRole('link', {name: 'Utility Pages'}).click();
+	await page.waitForTimeout(3000);
+
+
+
+
+	await page.locator('[id="_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_entries_5"]').getByLabel('More actions').click();
+	await page.getByRole('menuitem', { name: 'Delete' }).click();
+	await page.waitForTimeout(3000);
+	await page.getByRole('button', { name: 'Delete' }).click();
+
+	await page.getByLabel('Test Test User Profile').click();
+	await page.getByRole('menuitem', { name: 'Sign Out' }).click();
+	await page.getByRole('button', { name: 'Sign In' }).click();
+	await page.getByRole('menuitem', { name: 'Forgot Password' }).click();
+
+
+	expect(await page.getByRole('heading', { name: 'Forgot Password' }));
+})
+
+
+test('Create Account Utility Page created and set as default', async ({
+																		  page,
+																	  }) => {
+	await page.goto('/');
+	await page.getByLabel('Open Product Menu').click();
+	await page.getByRole('menuitem', { name: 'Site Builder' }).click();
+	await page.getByRole('menuitem', { name: 'Pages' }).click();
+	await page.getByRole('link', {name: 'Utility Pages'}).click();
+	await page.waitForTimeout(3000);
+	await page.getByRole('button', { name: 'New' }).click();
+	await page.getByRole('menuitem', { name: 'Create Account' }).click();
+	await page.getByRole('button', { name: 'Blank' }).click();
+	await page.getByPlaceholder('Name').click();
+	await page.getByPlaceholder('Name').fill('ca-test');
+	await page.getByRole('button', { name: 'Save' }).click();
+	await page.waitForTimeout(3000);
+	await page.getByRole('menuitem', { name: 'Button Add Button Mark Button as Favorite' }).click();
+	await page.getByRole('menuitem', { name: 'Button Add Button Mark Button as Favorite' }).press('Enter');
+
+	await page.getByRole('menuitem', { name: 'Button Add Button Mark Button as Favorite' }).locator('div').nth(1).click();
+	await page.getByLabel('Desktop', { exact: true }).press('Enter');
+	await page.getByRole('menuitem', { name: 'Button Add Button Mark Button as Favorite' }).click();
+	await page.getByLabel('Desktop', { exact: true }).press('Enter');
+	await page.getByLabel('Publish', { exact: true }).click();
+
+	page.on('dialog', async (dialogWindow) => {
+		await dialogWindow.accept();
+	});
+
+	await page.locator('[id="_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_entries_3"]').getByLabel('More actions').click();
+	await page.getByRole('menuitem', { name: 'Mark as Default' }).click();
+	await page.waitForTimeout(3000);
+
+
+
+
+	await page.getByLabel('Test Test User Profile').click();
+	await page.getByRole('menuitem', { name: 'Sign Out' }).click();
+	await page.getByRole('button', { name: 'Sign In' }).click();
+	await page.getByRole('link', { name: 'Create Account' }).click();
+
+
+	expect(await page.getByRole('link', { name: 'Go Somewhere' }));
+})
+
+test('Create Account Utility Page created and set as non-default 3', async ({
+																				page,
+																			}) => {
+	await page.goto('/');
+	await page.getByLabel('Open Product Menu').click();
+	await page.getByRole('menuitem', { name: 'Site Builder' }).click();
+	await page.getByRole('menuitem', { name: 'Pages' }).click();
+	await page.getByRole('link', {name: 'Utility Pages'}).click();
+	await page.waitForTimeout(3000);
+
+	await page.locator('[id="_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_entries_3"]').getByLabel('More actions').click();
+	await page.getByRole('menuitem', { name: 'Delete' }).click();
+	await page.waitForTimeout(3000);
+	await page.getByRole('button', { name: 'Delete' }).click();
+	await page.waitForTimeout(3000);
+
+	await page.getByLabel('Test Test User Profile').click();
+	await page.getByRole('menuitem', { name: 'Sign Out' }).click();
+	await page.getByRole('button', { name: 'Sign In' }).click();
+	await page.waitForTimeout(3000);
+	await page.getByRole('menuitem', { name: 'Create Account' }).click();
+
+	expect(await page.getByRole('heading', { name: 'Create Account' }));
 })
