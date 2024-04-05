f9eb84a67b1bf9a6e3caf01beb1c39009f2132ff
alvarosaugarlr <alvaro.saugar@liferay.com>
2024-04-05 02:25:14 +0200

    LPD-6871 - solving errors in the tests

--- modules/test/playwright/tests/sign-in-utility-page/sign-in-utility-page.spec.ts
+++ modules/test/playwright/tests/sign-in-utility-page/sign-in-utility-page.spec.ts
@@ -21,22 +21,23 @@ test('Forgot Pasword Utility Page created and set as default', async ({
 	await page.getByLabel('Open Product Menu').click();
 	await page.getByRole('menuitem', { name: 'Site Builder' }).click();
 	await page.getByRole('menuitem', { name: 'Pages' }).click();
-	await page.locator('li').filter({ hasText: 'Utility Pages' }).click();
+	await page.getByRole('link', {name: 'Utility Pages'}).click();
+	await page.waitForTimeout(3000);
 	await page.getByRole('button', { name: 'New' }).click();
 	await page.getByRole('menuitem', { name: 'Forgot Password' }).click();
 	await page.getByRole('button', { name: 'Blank' }).click();
 	await page.getByPlaceholder('Name').click();
 	await page.getByPlaceholder('Name').fill('fp-test');
-	await page.getByPlaceholder('Name').press('Enter');
-	await page.getByText('Button').click();
-	await page.getByText('Go Somewhere').dblclick();
-	await page.getByText('Go Somewhere').click();
-	await page.locator('#fragment-lfdm-link').dblclick();
-	await page.getByText('UP FP Hidden').click();
-	await page.getByLabel('Fragments and Widgets').click();
-	await page.getByLabel('Search Fragments and Widgets').click();
-	await page.getByLabel('Search Fragments and Widgets').fill('for');
-	await page.getByRole('menuitem', { name: 'Forgot Password Add Forgot Password Mark Forgot Password as Favorite' }).click();
+	await page.getByRole('button', { name: 'Save' }).click();
+	await page.waitForTimeout(3000);
+	await page.getByRole('menuitem', { name: 'Button Add Button Mark Button as Favorite' }).click();
+	await page.getByRole('menuitem', { name: 'Button Add Button Mark Button as Favorite' }).press('Enter');
+
+	await page.getByRole('menuitem', { name: 'Button Add Button Mark Button as Favorite' }).locator('div').nth(1).click();
+	await page.getByLabel('Desktop', { exact: true }).press('Enter');
+	await page.getByRole('menuitem', { name: 'Button Add Button Mark Button as Favorite' }).click();
+	await page.getByLabel('Desktop', { exact: true }).press('Enter');
+
 	await page.getByLabel('Publish', { exact: true }).click();
 	await page.locator('[id="_com_liferay_layout_admin_web_portlet_GroupPagesPortlet_entries_5"]').getByLabel('More actions').click();
 	await page.getByRole('menuitem', { name: 'Mark as Default' }).click();
@@ -44,11 +45,7 @@ test('Forgot Pasword Utility Page created and set as default', async ({
 	await page.getByRole('menuitem', { name: 'Sign Out' }).click();
 	await page.getByRole('button', { name: 'Sign In' }).click();
 	await page.getByRole('link', { name: 'Forgot Password' }).click();
-	await page.getByRole('link', { name: 'UP FP' }).click();
 
-	await expect(page.getByRole('link', { name: 'UP FP' }))
-		.toBeVisible({
-			timeout: 8 * 1000,
-		});
+
+	expect(await page.getByRole('link', { name: 'Go Somewhere' }));
 })
-
