9f13bb66287a5a2d6d8c85f939b8b326ab4dfa5f
Álvaro Saugar <alvaro.saugar@liferay.com>
2025-10-07 15:56:09 +0200

    MasterPageResourceImpl

--- modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/MasterPageResourceImpl.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/resource/v1_0/MasterPageResourceImpl.java
@@ -6,7 +6,10 @@
 package com.liferay.headless.admin.site.internal.resource.v1_0;
 
 import com.liferay.client.extension.type.manager.CETManager;
+import com.liferay.document.library.kernel.model.DLFolderConstants;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.util.ExportImportAttachmentManagerUtil;
 import com.liferay.exportimport.vulcan.batch.engine.ExportImportVulcanBatchEngineTaskItemDelegate;
 import com.liferay.headless.admin.site.dto.v1_0.ContentPageSpecification;
 import com.liferay.headless.admin.site.dto.v1_0.MasterPage;
@@ -23,9 +26,11 @@ import com.liferay.layout.page.template.constants.LayoutPageTemplateConstants;
 import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.layout.page.template.service.LayoutPageTemplateEntryService;
+import com.liferay.petra.io.StreamUtil;
 import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.search.filter.Filter;
@@ -44,6 +49,9 @@ import com.liferay.portal.vulcan.util.SearchUtil;
 
 import jakarta.ws.rs.core.MultivaluedMap;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -53,6 +61,20 @@ import java.util.Objects;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
+
+import com.liferay.headless.admin.site.dto.v1_0.URLReference;
+import com.liferay.exportimport.attachment.ExportImportAttachmentManager;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
+import com.liferay.portal.vulcan.fields.NestedField;
+import com.liferay.portal.vulcan.fields.NestedFieldId;
+import com.liferay.portal.kernel.util.ListUtil;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 /**
  * @author Rubén Pulido
@@ -68,8 +90,8 @@ public class MasterPageResourceImpl
 
 	@Override
 	public void deleteSiteMasterPage(
-			String siteExternalReferenceCode,
-			String masterPageExternalReferenceCode)
+		String siteExternalReferenceCode,
+		String masterPageExternalReferenceCode)
 		throws Exception {
 
 		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
@@ -104,7 +126,8 @@ public class MasterPageResourceImpl
 
 			@Override
 			public List<String> getNestedFields() {
-				return List.of("friendlyUrlHistory", "pageSpecifications");
+				return List.of(
+					"friendlyUrlHistory", "pageSpecifications", "thumbnail");
 			}
 
 			@Override
@@ -127,9 +150,9 @@ public class MasterPageResourceImpl
 
 	@Override
 	public ContentPageSpecification postSiteMasterPagePageSpecification(
-			String siteExternalReferenceCode,
-			String pageTemplateExternalReferenceCode,
-			ContentPageSpecification contentPageSpecification)
+		String siteExternalReferenceCode,
+		String pageTemplateExternalReferenceCode,
+		ContentPageSpecification contentPageSpecification)
 		throws Exception {
 
 		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
@@ -145,13 +168,13 @@ public class MasterPageResourceImpl
 						siteExternalReferenceCode));
 
 		if (!Objects.equals(
-				LayoutPageTemplateEntryTypeConstants.MASTER_LAYOUT,
-				layoutPageTemplateEntry.getType())) {
+			LayoutPageTemplateEntryTypeConstants.MASTER_LAYOUT,
+			layoutPageTemplateEntry.getType())) {
 
 			throw new UnsupportedOperationException();
 		}
 
-		return (ContentPageSpecification)_pageSpecificationDTOConverter.toDTO(
+		return (ContentPageSpecification) _pageSpecificationDTOConverter.toDTO(
 			LayoutUtil.addDraftToLayout(
 				_cetManager, contentPageSpecification,
 				_layoutLocalService.getLayout(
@@ -163,8 +186,8 @@ public class MasterPageResourceImpl
 
 	@Override
 	protected MasterPage doGetSiteMasterPage(
-			String siteExternalReferenceCode,
-			String masterPageExternalReferenceCode)
+		String siteExternalReferenceCode,
+		String masterPageExternalReferenceCode)
 		throws Exception {
 
 		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
@@ -180,8 +203,8 @@ public class MasterPageResourceImpl
 						siteExternalReferenceCode));
 
 		if (!Objects.equals(
-				LayoutPageTemplateEntryTypeConstants.MASTER_LAYOUT,
-				layoutPageTemplateEntry.getType())) {
+			LayoutPageTemplateEntryTypeConstants.MASTER_LAYOUT,
+			layoutPageTemplateEntry.getType())) {
 
 			throw new UnsupportedOperationException();
 		}
@@ -191,9 +214,9 @@ public class MasterPageResourceImpl
 
 	@Override
 	protected Page<MasterPage> doGetSiteMasterPagesPage(
-			String siteExternalReferenceCode, String search,
-			Aggregation aggregation, Filter filter, Pagination pagination,
-			Sort[] sorts)
+		String siteExternalReferenceCode, String search,
+		Aggregation aggregation, Filter filter, Pagination pagination,
+		Sort[] sorts)
 		throws Exception {
 
 		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
@@ -213,12 +236,12 @@ public class MasterPageResourceImpl
 			searchContext -> {
 				searchContext.setAttribute(
 					"types",
-					new String[] {
+					new String[]{
 						String.valueOf(
 							LayoutPageTemplateEntryTypeConstants.MASTER_LAYOUT)
 					});
 				searchContext.setCompanyId(contextCompany.getCompanyId());
-				searchContext.setGroupIds(new long[] {groupId});
+				searchContext.setGroupIds(new long[]{groupId});
 			},
 			sorts,
 			document -> _masterPageDTOConverter.toDTO(
@@ -226,9 +249,103 @@ public class MasterPageResourceImpl
 					GetterUtil.getLong(document.get(Field.ENTRY_CLASS_PK)))));
 	}
 
+	@NestedField(parentClass = MasterPage.class, value = "thumbnail")
+	public Page<URLReference> doGetSiteMasterPagesThumbnail(
+		String siteExternalReferenceCode,
+		@NestedFieldId(value = "externalReferenceCode") String
+			masterPageExternalReferenceCode)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
+			throw new UnsupportedOperationException();
+		}
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			_layoutPageTemplateEntryService.
+				getLayoutPageTemplateEntryByExternalReferenceCode(
+					masterPageExternalReferenceCode,
+					GroupUtil.getGroupId(
+						true, contextCompany.getCompanyId(),
+						siteExternalReferenceCode));
+/*
+		if (!Objects.equals(
+			LayoutPageTemplateEntryTypeConstants.MASTER_LAYOUT,
+			layoutPageTemplateEntry.getType())) {
+
+			throw new UnsupportedOperationException();
+		}
+
+
+ */
+		return Page.of(
+			_getPortletFileEntryURLReference(
+				layoutPageTemplateEntry.getPreviewFileEntryId()));
+	}
+
+	private List<URLReference>
+	_getPortletFileEntryURLReference(long fileEntryId)
+		throws Exception {
+
+
+
+
+	/*	URLReference urlReference new URLReference() {
+			{
+				setUrl(() -> _exportImportAttachmentManager.getFileURL(dlFileEntry));
+			}
+		};
+
+
+	 */
+		return ListUtil.fromArray(_toDTO(fileEntryId)
+		);
+
+	}
+
+
+	public URLReference _toDTO(long fileEntryId)
+		throws Exception {
+
+		if (fileEntryId <= 0) {
+			return null;
+		}
+
+		DLFileEntry dlFileEntry =
+			_dlFileEntryLocalService.getFileEntry(fileEntryId);
+
+		return new URLReference() {
+			{
+				setUrl(() -> _exportImportAttachmentManager.getFileURL(
+					dlFileEntry));
+			}
+		};
+
+	}
+
+	/*
+		private URLReference _toDTO(
+			DTOConverterContext dtoConverterContext,
+			LayoutPageTemplateEntry layoutPageTemplateEntry)
+			throws Exception {
+
+			Layout layout = _layoutLocalService.getLayout(
+				layoutPageTemplateEntry.getPlid());
+
+			return new URLReference() {
+				{
+					setThumbnail(
+						() ->
+							_getPortletFileEntryURLReference(
+								layoutPageTemplateEntry.getPreviewFileEntryId()));
+				}
+			};
+		}
+
+
+	 */
 	@Override
 	protected MasterPage doPostSiteMasterPage(
-			String siteExternalReferenceCode, MasterPage masterPage)
+		String siteExternalReferenceCode, MasterPage masterPage)
 		throws Exception {
 
 		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
@@ -244,8 +361,8 @@ public class MasterPageResourceImpl
 
 	@Override
 	protected MasterPage doPutSiteMasterPage(
-			String siteExternalReferenceCode,
-			String masterPageExternalReferenceCode, MasterPage masterPage)
+		String siteExternalReferenceCode,
+		String masterPageExternalReferenceCode, MasterPage masterPage)
 		throws Exception {
 
 		if (!FeatureFlagManagerUtil.isEnabled("LPD-35443")) {
@@ -254,6 +371,60 @@ public class MasterPageResourceImpl
 
 		long groupId = GroupUtil.getGroupId(
 			false, contextCompany.getCompanyId(), siteExternalReferenceCode);
+		URL uu = ExportImportAttachmentManagerUtil.getURL(
+			masterPage.getThumbnail().getUrl());
+		System.out.println(uu.toString());
+/*
+
+		URL url = _exportImportAttachmentManager.getURL(masterPage.getThumbnail().getUrl());
+
+
+
+		URLConnection urlConnection = url.openConnection();
+
+		if ((urlConnection instanceof
+			HttpURLConnection httpURLConnection) &&
+			(httpURLConnection.getResponseCode() !=
+			 HttpURLConnection.HTTP_OK)) {
+
+
+		}
+
+		byte[] bytee = StreamUtil.toByteArray(
+			urlConnection.getInputStream());
+
+		Path rutaGuardado = Paths.get("/home/me/Downloads/imagen_guardada.png"); // Reemplaza la ruta y nombre del archivo
+		try {
+			Files.write(rutaGuardado, bytee);
+			System.out.println("Imagen guardada correctamente.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/// ///////////////////////////////////
+
+
+		//long userId = // user que crea el archivo ;
+		//long groupId = // site groupId, suele ser repositoryId ;
+		long repositoryId = groupId;
+		long folderId = DLFolderConstants.DEFAULT_PARENT_FOLDER_ID; // o tu carpeta
+		String fileName = "imagen.png";
+		String title = "Miniatura";
+		String description = "Subido desde bytes";
+		String changeLog = "";
+		//String mimeType = MimeTypesUtil.getContentType(fileName); // o calcula por contenido
+
+		//byte[] bytes = // tu array ;
+
+		ServiceContext sc = new ServiceContext();
+		sc.setScopeGroupId(groupId);
+// Permisos (ajusta según tu caso):
+		sc.setAddGroupPermissions(true);
+		sc.setAddGuestPermissions(false);
+
+		FileEntry fileEntry = _dlAppLocalService.addFileEntry(siteExternalReferenceCode, contextUser.getUserId(), repositoryId, folderId, getResourceName(), null, bytee, null, null, null, null);
+
+*/
 
 		LayoutPageTemplateEntry layoutPageTemplateEntry =
 			_layoutPageTemplateEntryService.
@@ -264,11 +435,68 @@ public class MasterPageResourceImpl
 			return _addMasterPage(groupId, masterPage);
 		}
 
-		long previewFileEntryId = FileEntryUtil.getPreviewFileEntryId(
-			groupId, masterPage.getThumbnail());
+
+		URL url = _exportImportAttachmentManager.getURL(
+			masterPage.getThumbnail().getUrl());
+
+
+		URLConnection urlConnection = url.openConnection();
+
+		if ((urlConnection instanceof
+			HttpURLConnection httpURLConnection) &&
+			(httpURLConnection.getResponseCode() !=
+			 HttpURLConnection.HTTP_OK)) {
+
+
+		}
+
+		byte[] bytee = StreamUtil.toByteArray(
+			urlConnection.getInputStream());
+
+		/// ///////////////////////////////////
+
+
+		long repositoryId = groupId;
+		long folderId =
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID; // o tu carpeta
+		String fileName = "imagen.png";
+		String title = "Miniatura";
+		String description = "Subido desde bytes";
+		String changeLog = "";
+		//String mimeType = MimeTypesUtil.getContentType(fileName); // o calcula por contenido
+
+
+		ServiceContext sc = new ServiceContext();
+		sc.setScopeGroupId(groupId);
+// Permisos (ajusta según tu caso):
+		sc.setAddGroupPermissions(true);
+		sc.setAddGuestPermissions(false);
+
+		FileEntry fileEntry =
+			_dlAppLocalService.addFileEntry(
+				siteExternalReferenceCode,
+				contextUser.getUserId(), repositoryId, folderId,
+				getResourceName(), null, bytee, null, null, null, null);
+
+		/*
+		FileEntry fileEntry = _dlAppLocalService.addFileEntry(
+			contextUser.getUserId(), repositoryId, folderId,
+			fileName, null, title, description, changeLog,
+			bytee, sc);
+
+
+		 */
+
+
+		/// /////////////////////
+
+
+		//	long previewFileEntryId = FileEntryUtil.getPreviewFileEntryId(
+		//		groupId, masterPage.getThumbnail());
+		long previewFileEntryId = fileEntry.getFileEntryId();
 
 		if (previewFileEntryId !=
-				layoutPageTemplateEntry.getPreviewFileEntryId()) {
+			layoutPageTemplateEntry.getPreviewFileEntryId()) {
 
 			layoutPageTemplateEntry =
 				_layoutPageTemplateEntryService.updateLayoutPageTemplateEntry(
@@ -295,8 +523,8 @@ public class MasterPageResourceImpl
 		}
 
 		if (!Objects.equals(
-				GetterUtil.getBoolean(masterPage.getMarkedAsDefault()),
-				layoutPageTemplateEntry.isDefaultTemplate())) {
+			GetterUtil.getBoolean(masterPage.getMarkedAsDefault()),
+			layoutPageTemplateEntry.isDefaultTemplate())) {
 
 			layoutPageTemplateEntry =
 				_layoutPageTemplateEntryService.updateLayoutPageTemplateEntry(
@@ -319,7 +547,7 @@ public class MasterPageResourceImpl
 
 	@Override
 	protected Long getPermissionCheckerResourceId(
-			String groupExternalReferenceCode, String externalReferenceCode)
+		String groupExternalReferenceCode, String externalReferenceCode)
 		throws Exception {
 
 		LayoutPageTemplateEntry layoutPageTemplateEntry =
@@ -333,7 +561,7 @@ public class MasterPageResourceImpl
 
 	@Override
 	protected String getPermissionCheckerResourceName(
-			String groupExternalReferenceCode, String externalReferenceCode)
+		String groupExternalReferenceCode, String externalReferenceCode)
 		throws Exception {
 
 		return LayoutPageTemplateEntry.class.getName();
@@ -373,6 +601,52 @@ public class MasterPageResourceImpl
 
 		ServiceContext serviceContext = _getServiceContext(groupId, masterPage);
 
+
+		URL url = _exportImportAttachmentManager.getURL(
+			masterPage.getThumbnail().getUrl());
+
+
+		URLConnection urlConnection = url.openConnection();
+
+		if ((urlConnection instanceof
+			HttpURLConnection httpURLConnection) &&
+			(httpURLConnection.getResponseCode() !=
+			 HttpURLConnection.HTTP_OK)) {
+
+
+		}
+
+		byte[] bytee = StreamUtil.toByteArray(
+			urlConnection.getInputStream());
+
+		/// ///////////////////////////////////
+
+
+		long repositoryId = groupId;
+		long folderId =
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID; // o tu carpeta
+		String fileName = "imagen.png";
+		String title = "Miniatura";
+		String description = "Subido desde bytes";
+		String changeLog = "";
+		//String mimeType = MimeTypesUtil.getContentType(fileName); // o calcula por contenido
+
+
+		ServiceContext sc = new ServiceContext();
+		sc.setScopeGroupId(groupId);
+// Permisos (ajusta según tu caso):
+		sc.setAddGroupPermissions(true);
+		sc.setAddGuestPermissions(false);
+
+		FileEntry fileEntry =
+			_dlAppLocalService.addFileEntry(
+				masterPage.getExternalReferenceCode(),
+				contextUser.getUserId(), repositoryId, folderId,
+				getResourceName(), null, bytee, null, null, null, serviceContext);
+
+
+
+
 		return _masterPageDTOConverter.toDTO(
 			_layoutPageTemplateEntryService.addLayoutPageTemplateEntry(
 				masterPage.getExternalReferenceCode(), groupId,
@@ -380,8 +654,7 @@ public class MasterPageResourceImpl
 					PARENT_LAYOUT_PAGE_TEMPLATE_COLLECTION_ID_DEFAULT,
 				masterPage.getKey(), 0, 0, masterPage.getName(),
 				LayoutPageTemplateEntryTypeConstants.MASTER_LAYOUT,
-				FileEntryUtil.getPreviewFileEntryId(
-					groupId, masterPage.getThumbnail()),
+				fileEntry.getFileEntryId(),
 				defaultTemplate, 0,
 				_getLayoutPlid(groupId, masterPage, serviceContext), 0,
 				PageSpecificationUtil.getPublishedStatus(
@@ -390,7 +663,7 @@ public class MasterPageResourceImpl
 	}
 
 	private long _getLayoutPlid(
-			long groupId, MasterPage masterPage, ServiceContext serviceContext)
+		long groupId, MasterPage masterPage, ServiceContext serviceContext)
 		throws Exception {
 
 		Map<Locale, String> nameMap = Collections.singletonMap(
@@ -413,7 +686,7 @@ public class MasterPageResourceImpl
 	}
 
 	private ServiceContext _getServiceContext(
-			long groupId, MasterPage masterPage)
+		long groupId, MasterPage masterPage)
 		throws Exception {
 
 		return ServiceContextUtil.createServiceContext(
@@ -449,5 +722,15 @@ public class MasterPageResourceImpl
 
 	@Reference
 	private Portal _portal;
+
+	@Reference
+	private DLFileEntryLocalService _dlFileEntryLocalService;
+
+	@Reference
+	private ExportImportAttachmentManager _exportImportAttachmentManager;
+
+	@Reference
+	private com.liferay.document.library.kernel.service.DLAppLocalService
+		_dlAppLocalService;
 
 }
